380d4b65a88276b71a1d7f53f55833eec763ffef
Ronald-TR <ronald-farias@outlook.com>
2022-04-24 10:54:57 -0300

    refactor: variables1 with all immutable types

--- pythonlings/exercises/1_variables/variables1.py
+++ pythonlings/exercises/1_variables/variables1.py
@@ -1,9 +1,15 @@
 # I AM NOT DONE
 
-x = 1
-y = 0.1
-z = "Hello World!"
+a_integer = 1
+a_float = 0.1
+a_string = "Hello World!"
+a_bytes = b"Hello"
+a_boolean = True
+a_tuple = (1, 2, "3", False)
 
-assert isinstance(x, int)
-assert isinstance(y, float)
-assert isinstance(z, str)
+assert isinstance(a_integer, int)
+assert isinstance(a_float, float)
+assert isinstance(a_string, str)
+assert isinstance(a_bytes, bytes)
+assert isinstance(a_boolean, True)
+assert isinstance(a_tuple, tuple)
