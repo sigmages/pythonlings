17ba7d6952b9a4e4261f87a7b343996d5b3441a0
Ronald-TR <ronald-farias@outlook.com>
2022-04-26 01:33:41 -0300

    refactor: data_structures1

--- pythonlings/exercises/2_data_structures/data_structures1.py
+++ pythonlings/exercises/2_data_structures/data_structures1.py
@@ -1,5 +1,14 @@
-dictionary1 = {}
-dictionary2 = dict()
+# I AM NOT DONE
 
-assert isinstance(dictionary1, dict)
-assert isinstance(dictionary2, dict)
+my_list = [0, "hello", True, ["other list"]] # or my_list = list()
+
+my_list[0] = 1
+my_list.append(3)
+my_list.append(4)
+my_list.pop()
+
+# DON'T EDIT THE TESTS
+assert len(my_list) == 4
+assert my_list[0] == 1
+assert my_list[1] == "world"
+assert my_list[2] == False
