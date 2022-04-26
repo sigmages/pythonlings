74a956a85b49c2b79ce0be4279b8bb24ac062bbe
Ronald-TR <ronald-farias@outlook.com>
2022-04-25 23:15:49 -0300

    feat: variables5

---
+++ pythonlings/exercises/1_variables/variables5.py
@@ -0,0 +1,13 @@
+# I AM NOT DONE
+
+hello_immutable_string = "Hello"
+
+hello_mutable_list = ["H", "e", "l", "l", "o"]
+hello_mutable_dict = {"Hello": "World"}
+hello_mutable_set = {1, 2, 3}
+
+hello_immutable_string[0] = "H"
+hello_mutable_list[0] = "I WORK"
+
+# DON'T EDIT THE TESTS
+assert "".join(hello_mutable_list) == "I WORKello"
