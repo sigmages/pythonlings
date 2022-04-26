e9032be1c9cb936126775e6397e127ff52bcdbc2
Ronald <ronald-farias@outlook.com>
2022-04-25 23:28:14 -0300

    WIP: Improvement/variables (#12)
    * feat: variables3
    
    * fix: typo
    
    * refactor: variables
    
    * refactor: variables1 with all immutable types
    
    * feat: variables5
    
    * feat: variables5
    
    * refactor: variables5

---
+++ pythonlings/exercises/1_variables/variables3.py
@@ -0,0 +1,10 @@
+# I AM NOT DONE
+
+x = y = z = "Hello!"
+a, b, c = "a", "b", "c"
+
+# DON'T EDIT THE TESTS
+assert x == y == z == "Hello!"
+assert a == "a"
+assert b == "b"
+assert c == "c"
---
+++ pythonlings/exercises/1_variables/variables4.py
@@ -0,0 +1,13 @@
+# I AM NOT DONE
+
+hello = "Hello "
+world = "World!"
+hello_world = hello + world
+
+one = 1
+two = "2"
+one_plus_two = one + two
+
+# DON'T EDIT THE TESTS
+assert hello_world == "Hello World!"
+assert one_plus_two == 3
